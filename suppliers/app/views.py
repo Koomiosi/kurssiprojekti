8ade6419c061fafdbee9287d4710719ec5cefe36
Koomis <tonykelanen@gmail.com>
2025-09-07 21:23:46 +0300

    Suppliers list view added and a few buttons

--- suppliers/app/views.py
+++ suppliers/app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from .models import Supplier, Product
 
 def landingview(request):
     return render(request, 'landingpage.html')
 
 def supplierslistview(request):
-    return render(request, 'supplierslist.html')
+    supplierlist = Supplier.objects.all()
+    context = {'suppliers': supplierlist}
+    return render(request, 'supplierslist.html', context)
 
 def productslistview(request):
-    return render(request, 'productslist.html')
+    productlist = Product.objects.all()
+    supplierlist = Supplier.objects.all()
+    context = {'products': productlist, 'suppliers': supplierlist}
+    return render(request, 'productslist.html', context)
