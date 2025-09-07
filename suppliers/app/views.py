f82cdd6e508d84e34f2be02754fee55dc7e14533
Koomis <tonykelanen@gmail.com>
2025-09-07 08:10:12 +0300

    Added products and suppliers pages

--- suppliers/app/views.py
+++ suppliers/app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
-def landing_page(request):
+def landingview(request):
     return render(request, 'landingpage.html')
+
+def supplierslistview(request):
+    return render(request, 'supplierslist.html')
+
+def productslistview(request):
+    return render(request, 'productslist.html')
